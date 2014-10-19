00aef707e4fa841f35834893bbf775c19cc843e1
newmlee <matthewlee0725@gmail.com>
2014-10-19 23:38:25 +0000

    完成About界面，达成1.0.0版本

--- Tweetin/src/io/github/mthli/Tweetin/About/AboutActivity.java
+++ Tweetin/src/io/github/mthli/Tweetin/About/AboutActivity.java
@@ -1,12 +1,16 @@
 package io.github.mthli.Tweetin.About;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 import io.github.mthli.Tweetin.R;
 import io.github.mthli.Tweetin.Unit.ActivityAnim;
@@ -26,7 +30,15 @@ public class AboutActivity extends Activity {
         }
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        /* Do something */
+        Button button = (Button) findViewById(R.id.about_github);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse(getString(R.string.about_github_url));
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
