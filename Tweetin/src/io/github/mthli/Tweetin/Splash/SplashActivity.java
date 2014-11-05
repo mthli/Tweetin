dbdb8849c4c2d4b00015fc7e12b80c6760c76623
newmlee <matthewlee0725@gmail.com>
2014-11-05 18:22:36 +0000

    增加Ripple效果

--- Tweetin/src/io/github/mthli/Tweetin/Splash/SplashActivity.java
+++ Tweetin/src/io/github/mthli/Tweetin/Splash/SplashActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -16,6 +17,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.*;
+import com.balysv.materialripple.MaterialRippleLayout;
 import io.github.mthli.Tweetin.Main.MainActivity;
 import io.github.mthli.Tweetin.R;
 import twitter4j.Twitter;
@@ -216,6 +218,13 @@ public class SplashActivity extends Activity {
         final EditText conSecretEdit = (EditText) findViewById(R.id.splash_consumer_secret);
 
         Button signIn = (Button) findViewById(R.id.splash_sign_in);
+        MaterialRippleLayout.on(signIn)
+                .rippleOverlay(true)
+                .rippleColor(getResources().getColor(R.color.black))
+                .rippleAlpha(0.1f)
+                .rippleDiameterDp(10)
+                .rippleDuration(350)
+                .create();
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
